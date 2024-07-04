58dc10b3b157163b6ce9c9bba075ffd68d301257
Ankit Panigrahi <panigrahi0702@gmail.com>
2024-07-04 10:16:23 +0530

    Fixed the navbar and footer, also fixed the cards.

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const searchBar = document.getElementById("search-bar1");
   const resultsContainer = document.getElementById("results");
+  let data = [];
 
-  // Fetch all items and display them initially
   fetch("https://fakestoreapi.com/products")
     .then((response) => response.json())
-    .then((data) => {
+    .then((responseData) => {
+      data = responseData;
       displayResults(data);
     });
 
@@ -13,22 +14,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const query = searchBar.value.trim().toLowerCase();
 
     if (query.length > 0) {
-      fetch("https://fakestoreapi.com/products")
-        .then((response) => response.json())
-        .then((data) => {
-          const filteredItems = data.filter((item) => {
-            return item.title.toLowerCase().includes(query);
-          });
+      const filteredItems = data.filter((item) => {
+        return item.title.toLowerCase().includes(query);
+      });
 
-          displayResults(filteredItems);
-        });
+      displayResults(filteredItems);
     } else {
-      // If search bar is empty, display all items
-      fetch("https://fakestoreapi.com/products")
-        .then((response) => response.json())
-        .then((data) => {
-          displayResults(data);
-        });
+      displayResults(data);
     }
   });
 
@@ -39,16 +31,31 @@ document.addEventListener("DOMContentLoaded", () => {
       const itemDiv = document.createElement("div");
       itemDiv.classList.add("item");
 
-      //template string
       itemDiv.innerHTML = ` 
-        <img src="${item.image}" alt="${item.title}" class="prod-image" />
-        <p class="category">${
-          item.category.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
-        }</p>
-        <h3 class="title">${item.title}</h3>
+      <div>
+          <div class="prod-image">
+          <img src="${item.image}" alt="${item.title}" class="image" />
+        </div>
+
+        <p class="category">${item.category
+          .split(" ")
+          .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(" ")}</p>
+        
+        <div class="title-text">
+          <h3 class="title">${item.title.substring(0, 30)}</h3>
+        </div>
+
         <p class="description">${item.description.substring(0, 50)}</p>
-        <p>${item.rating.rate}</p>
-        <p>${item.price}</p>
+
+        <div class="rating">
+          <p>${item.rating.rate}</p>
+        </div>
+
+        <div class="price">
+          <p>$${item.price}</p>
+        </div>
+      </div>
         `;
 
       resultsContainer.appendChild(itemDiv);
